690c9450ed0178e16f5234fb9b482e3258b3df7f
dmitriy337 <50007037+vuhiza@users.noreply.github.com>
2021-11-10 13:31:57 +0200

    init

---
+++ src/settings/__init__.py
@@ -0,0 +1,2 @@
+from .settings import *
+from .requests_settings import *
---
+++ src/settings/settings.py
@@ -0,0 +1,28 @@
+BOT_NAME = 'scrapy_vuhizas_boilerplate'
+
+SPIDER_MODULES = ['spiders']
+NEWSPIDER_MODULE = 'spiders'
+
+
+ROBOTSTXT_OBEY = False
+COOKIES_ENABLED = False
+
+TELNETCONSOLE_ENABLED = False
+
+# SPIDER_MIDDLEWARES = {
+#     'scrapy_vuhizas_boilerplate.middlewares.ScrapyVuhizasBoilerplateSpiderMiddleware': 543,
+# }
+
+
+
+# Enable or disable extensions
+# See https://docs.scrapy.org/en/latest/topics/extensions.html
+#EXTENSIONS = {
+#    'scrapy.extensions.telnet.TelnetConsole': None,
+#}
+
+# Configure item pipelines
+# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+#ITEM_PIPELINES = {
+#    'scrapy_vuhizas_boilerplate.pipelines.ScrapyVuhizasBoilerplatePipeline': 300,
+#}
